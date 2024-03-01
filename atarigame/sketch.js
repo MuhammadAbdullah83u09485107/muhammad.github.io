7ccb8e0ff6c8af4b2ad9cafad1b8f39aaf902727
MuhammadAbdullah83u09485107 <161519970+MuhammadAbdullah83u09485107@users.noreply.github.com>
2024-03-01 13:53:00 -0600

    added interactive scene

---
+++ atarigame/sketch.js
@@ -0,0 +1,18 @@
+let rectSize = 65;
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  draw_base();
+}
+
+//drawing base
+function draw_base() {
+  rect(mouseX - rectSize/2, 350, rectSize, 15);
+  if (mouseX - rectSize/2 < width || mouseX - rectSize/2 > width) {
+    
+  }
+}
--- index.md
+++ index.md
@@ -1,1 +1,2 @@
 # Traffic Light
+- [Interactive Scene](atarigame)
