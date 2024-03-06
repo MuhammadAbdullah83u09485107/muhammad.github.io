889b8c2fa1bd5290b74b5111e9e84d310c8f828f
MuhammadAbdullah83u09485107 <161519970+MuhammadAbdullah83u09485107@users.noreply.github.com>
2024-03-06 14:08:38 -0600

    added sprite

--- atarigame/sketch.js
+++ atarigame/sketch.js
@@ -1,33 +1,33 @@
 let rectSize = 65;
 circleSize = 20;
-let rectX = mouseX - rectSize/2;
-let sqrs;
+let rectX;
 
 function setup() {
   createCanvas(400, 400);
-  noStroke;
+  noStroke();
+}
+
+function preload() {
+  sqrs = loadImage("atarirect.png");
 }
 
 function draw() {
-
+  background(225);
   setupGame();
 }
 
 
 function setupGame() {
   //create pad
+  let rectX = mouseX - rectSize/2;
   if (rectX > 0 || rectX < width) {
     rect(rectX, 350, rectSize, 15);
   }
   //make ball
   let x = 1;
   circle(mouseX, 340 + x, circleSize)
-  fill("red")
   //create rectangles
-  rect(random(height/2), random(width), random(50, 70), random(50, 70));
-}
-
-function preload() {
-  sqrs = loadImage()
+  // rect(random(width), random(height), random(50, 70), random(50, 70));
+  //  fill("red")
 }
 
