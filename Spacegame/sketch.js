195df264483843b0d177b712ea02d7220c7c4f13
MuhammadAbdullah83u09485107 <161519970+MuhammadAbdullah83u09485107@users.noreply.github.com>
2024-04-07 12:30:38 -0600

    try to make spacedhip move

--- Spacegame/sketch.js
+++ Spacegame/sketch.js
@@ -9,6 +9,9 @@ let bulletArray = [];
 let x;
 let y;
 let spaceship;
+let xship;
+let yship;
+
 
 function preload() {
   spaceship = loadImage("spaceship-23.png");
@@ -23,11 +26,12 @@ function draw() {
   background("white");
   displayBullets();
   moveBullets();
-  image(spaceship, (windowWidth/2), (windowHeight/2), spaceship.width, spaceship.height);
+  displayShip();
+  moveSpaceship();
 }
 
 function displayBullets() {
-  for (let bullet of bulletArray) {
+    for (let bullet of bulletArray) {
     fill(bullet.r, bullet.g, bullet.b);
     circle(bullet.x, bullet.y, bullet.size);
   }
@@ -37,7 +41,7 @@ function spawnBullets() {
   let bullet = {
     x: windowWidth/2,
     y: windowHeight/2,
-    speed: 20,
+    speed: 1,
     deltaTime: 0.01,
     size: 10,
     r: 255,
@@ -60,14 +64,33 @@ function moveBulletUp(bullet) {
   while (i < 10) {
     bullet.y -= bullet.speed;
   }
-  // const intervalId = setInterval(function() {
-  //   bullet.y -= bullet.speed;
-  //   i++;
-  //   if (i >= height) {
-  //     clearInterval(intervalId);
+   const intervalId = setInterval(function() {
+     bullet.y -= bullet.speed;
+     i++;
+     if (i >= height) {
+       clearInterval(intervalId);
     }
-  // }, 1000);
-// }
+   }, 1000);
+ }
 
-
-
+ function moveSpaceship() {
+  if (keyIsDown(87)) { //w
+    yship -= 1;
+  if (keyIsDown(83)) { //s
+    yship += 1;
+  }
+  if (keyIsDown(68)) { //d
+    xship += 1;
+  }
+  if (keyIsDown(65)) { //a
+    xship -= 1;
+  }
+}
+ }
+ 
+ function displayShip() {
+  xship = width / 2;
+  yship = height / 2;
+  console.log(yship)
+  image(spaceship, xship, yship, spaceship.width, spaceship.height);
+}
