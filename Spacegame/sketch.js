33bb48dcb72ebeebadd0014c99705e508426e92c
MuhammadAbdullah83u09485107 <161519970+MuhammadAbdullah83u09485107@users.noreply.github.com>
2024-04-08 20:35:10 -0600

    added comments

--- Spacegame/sketch.js
+++ Spacegame/sketch.js
@@ -3,7 +3,7 @@
 // March 22, 2024
 //
 // Extra for Experts:
-// - ?
+// - used a constraint function to keep the spaceship within the canvas, and also added collision detection  by having the bullet and the alien dissapear after they make contact.
 
 let bulletArray = [];
 let shipArray = [];
@@ -48,6 +48,7 @@ function draw() {
 }
 
 function collisionDetection(){
+  //removes bullet and alien if they hit each other
   if(bulletArray.length >= 1){
     for (let [index, alienItem] of alienArray.entries()){
       for (let [i, bullet] of bulletArray.entries()) {
@@ -62,7 +63,7 @@ function collisionDetection(){
 }
 
 function displayScore() {
-  //keeps track of score in top left corner
+  //shows scorein top left
   fill(255);
   textSize(20);
   textAlign(LEFT, TOP);
@@ -70,6 +71,7 @@ function displayScore() {
 }
 
 function displayAliens() {
+  //draws a row of aliens
     for (let i = 0; i < alienArray.length; i++) {
       let alien = alienArray[i];
       image(aliens, alien.x, alien.y);
@@ -78,6 +80,7 @@ function displayAliens() {
   
 
 function displayBullets() {
+  //draws the bullets that get shot out of the spaceship
   for (let i = bulletArray.length - 1; i >= 0; i--) {
     let bullet = bulletArray[i];
     fill(bullet.r, bullet.g, bullet.b);
@@ -86,6 +89,7 @@ function displayBullets() {
 }
 
 function spawnBullets(x, y) {
+  //object holding the properties of the bullets
   let bullet = {
     x: x + spaceship.width / 2,
     y: y,
@@ -99,6 +103,7 @@ function spawnBullets(x, y) {
 }
 
 function moveBullets() {
+  //shoots bullet and removes it if it goes off screen
   for (let i = bulletArray.length - 1; i >= 0; i--) {
     let bullet = bulletArray[i];
     bullet.y -= bullet.speed;
@@ -109,11 +114,13 @@ function moveBullets() {
 }
 
 function displayShip() {
+  //draws ship
   image(spaceship, xship, yship);
 }
 
 
 function moveSpaceship() {
+  //moves the ship left and right
   if (keyIsDown(68)) {
     // 'd'
     isMoving = true;
@@ -125,34 +132,38 @@ function moveSpaceship() {
   } else {
     isMoving = false;
   }
-  xship = constrain(xship, 0, width - spaceship.width); // Constrain spaceship within canvas
+  xship = constrain(xship, 0, width - spaceship.width); // Keep spaceship within canvas
 }
 
 function spawnSpaceship() {
+  //decidedes the position of the spaceship on the canvas
   xship = windowWidth / 3 - spaceship.width / 2;
   yship = windowHeight - 60;
 }
 
 function keyPressed() {
+  //does not let the player shoot bullet while moving the spaceship
   if (keyCode === UP_ARROW && !isMoving) {
     shootBulletUp();
   }
 }
 
 function shootBulletUp() {
+  //choose the locations that the bullets spawn from
   spawnBullets(xship - 20, yship);
   spawnBullets(xship + 20, yship); // Spawn second bullet with an x offset of 10 pixels
 }
 
 function spawnAliens() {
-  // Spawn aliens in 3 columns and rows of 5
+  // Spawn aliens in a row of 7
   for (let i = 0; i < 7; i++) {
     for (let j = 0; j < 1; j++) {
       let xcomp = (i + 1) * (width / 8) - alienWidth / 2;
       let ycomp = (j + 1) * (height / 8) - alienHeight / 2;
       let alien = {
-        x: xcomp, // Spread aliens evenly in 3 columns
-        y: ycomp, // Spread aliens evenly in 5 rows
+        //makes an "alien" object that stores the coordinates of the aliens that make collision detection possible
+        x: xcomp, 
+        y: ycomp, 
         x1: xcomp-25,
         x2: xcomp+25,
         y1: ycomp-25,
