471a4a6adda1c0a0a71cf2b32a2e6d84685b3e5c
MuhammadAbdullah83u09485107 <161519970+MuhammadAbdullah83u09485107@users.noreply.github.com>
2024-04-08 19:43:35 -0600

    Finished Game

--- Spacegame/sketch.js
+++ Spacegame/sketch.js
@@ -5,150 +5,77 @@
 // Extra for Experts:
 // - ?
 
-// let bulletArray = [];
-// let shipArray = [];
-// let x;
-// let y;
-// let spaceship;
-// let xship = 0;
-// let yship = 0;
-// let shipSpeed = 5;
-// let speed;
-
-
-// function preload() {
-//   spaceship = loadImage("spaceship-23.png");
-// }
-
-// function setup() {
-//   createCanvas(windowWidth, windowHeight);
-//   spawnSpaceship();
-//   spawnBullets();
-// }
-
-// function draw() {
-//   background("white");
-//   displayBullets();
-//   //moveBullets();
-//   displayShip();
-//   moveSpaceship();
-//   shootBulletUp();
-// }
-
-// function displayBullets() {
-//     for (let bullet of bulletArray) {
-//     fill(bullet.r, bullet.g, bullet.b);
-//     console.log(bullet.x, bullet.y);
-//     circle(bullet.x, bullet.y, bullet.size);
-//   }
-// }
-
-// function spawnBullets() {
-//   let bullet = {
-//     x: xship + (spaceship.width/3),
-//     y: yship + (spaceship.height/2),
-//     speed: 1,
-//     deltaTime: 0.01,
-//     size: 10,
-//     r: 255,
-//     g: 0,
-//     b: 0,
-//   }
-//   bulletArray.push(bullet);
-// }
-
-// function moveBullets() {
-//   if (keyIsPressed && keyCode === UP_ARROW) {
-//     for (let bullet of bulletArray) {
-//       moveBulletUp(bullet);
-//     }
-//   }
-// }
-
-
-// function moveBulletUp(bullet) {
-//   let i = 0;
-//   while (i < 10) {
-//     bullet.y -= bullet.speed;
-//   }
-//    const intervalId = setInterval(function() {
-//      bullet.y -= bullet.speed;
-//      i++;
-//      if (i >= height) {
-//        clearInterval(intervalId);
-//     }
-//    }, 100);
-//  }
- 
-//  function displayShip() {
-//   for (ship of shipArray) {
-//     image(spaceship, ship.x, ship.y, spaceship.width, spaceship.height);
-//   }
-// }
-
-// function moveSpaceship() {
-//   if (keyIsDown(68)) { //d
-//     for (ship of shipArray) {
-//     ship.x+= shipSpeed;
-//     xship += shipSpeed;
-//     }
-//   }
-// else if (keyIsDown(65)) { //a
-//   for (ship of shipArray) {
-//     ship.x -= shipSpeed;
-//     xship -= shipSpeed;
-//   }
-//   }
-// }
-
-// function spawnSpaceship() {
-//   let ship = {
-//   x: windowWidth/2,
-//   y: windowHeight - spaceship.height,
-//   }
-//   xship = ship.x;
-//   yship = ship.y;
-//   shipArray.push(ship);
-// }
-
-// function shootBulletUp() {
-//   if (keyIsPressed && keyCode === UP_ARROW) {
-//     for (bullet of bulletArray) {
-//       shootBullet(bullet);
-//     }
-//   }
-// }
-
-// function shootBullet(bullet) {
-//   for (bullet of bulletArray) {
-//     bullet.y -= bullet.speed;
-//   }
-// }
-
 let bulletArray = [];
 let shipArray = [];
+let alienArray = [];
 let spaceship;
+let bImage;
 let xship = 0;
 let yship = 0;
 let shipSpeed = 5;
 let isMoving = false; // Boolean variable to track spaceship movement
+let aliens;
+let alienWidth = 30; // Width of the resized alien image
+let alienHeight = 20; // Height of the resized alien image
+let score;
 
 function preload() {
   spaceship = loadImage("spaceship-23.png");
+  bImage = loadImage("Spacebackground.png");
+  aliens = loadImage("alien.png");
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   spawnSpaceship();
+  spaceship.resize(60, 0); // Resize the spaceship image
+  spawnAliens();
+  aliens.resize(alienWidth, alienHeight); // Resize the alien image
+  spawnAliens();
+  score = 0;
 }
 
 function draw() {
-  background(220);
+  background(bImage);
   displayBullets();
   displayShip();
   moveSpaceship();
   moveBullets();
+  displayAliens();
+  displayScore();
+  collisionDetection()
+  
 }
+
+function collisionDetection(){
+  if(bulletArray.length >= 1){
+    for (let [index, alienItem] of alienArray.entries()){
+      for (let [i, bullet] of bulletArray.entries()) {
+        if ((alienItem.y2 >= bullet.y && bullet.y >= alienItem.y1) && (alienItem.x2 >= bullet.x && bullet.x >= alienItem.x1)){
+          bulletArray.splice(i, 1); 
+          alienArray.splice(index, 1); 
+          score++;
+        }
+      }
+  }
+  }
+}
+
+function displayScore() {
+  //keeps track of score in top left corner
+  fill(255);
+  textSize(20);
+  textAlign(LEFT, TOP);
+  text("Score: " + score, 10, 10);
+}
+
+function displayAliens() {
+    for (let i = 0; i < alienArray.length; i++) {
+      let alien = alienArray[i];
+      image(aliens, alien.x, alien.y);
+    }
+  }
+  
 
 function displayBullets() {
   for (let i = bulletArray.length - 1; i >= 0; i--) {
@@ -163,7 +90,7 @@ function spawnBullets(x, y) {
     x: x + spaceship.width / 2,
     y: y,
     speed: 5,
-    size: 10,
+    size: 5,
     r: 255,
     g: 0,
     b: 0,
@@ -185,6 +112,7 @@ function displayShip() {
   image(spaceship, xship, yship);
 }
 
+
 function moveSpaceship() {
   if (keyIsDown(68)) {
     // 'd'
@@ -202,7 +130,7 @@ function moveSpaceship() {
 
 function spawnSpaceship() {
   xship = windowWidth / 3 - spaceship.width / 2;
-  yship = windowHeight - spaceship.height;
+  yship = windowHeight - 60;
 }
 
 function keyPressed() {
@@ -212,6 +140,27 @@ function keyPressed() {
 }
 
 function shootBulletUp() {
-  spawnBullets(xship, yship);
-  spawnBullets(xship + 30, yship); // Spawn second bullet with an x offset of 10 pixels
+  spawnBullets(xship - 20, yship);
+  spawnBullets(xship + 20, yship); // Spawn second bullet with an x offset of 10 pixels
 }
+
+function spawnAliens() {
+  // Spawn aliens in 3 columns and rows of 5
+  for (let i = 0; i < 7; i++) {
+    for (let j = 0; j < 1; j++) {
+      let xcomp = (i + 1) * (width / 8) - alienWidth / 2;
+      let ycomp = (j + 1) * (height / 8) - alienHeight / 2;
+      let alien = {
+        x: xcomp, // Spread aliens evenly in 3 columns
+        y: ycomp, // Spread aliens evenly in 5 rows
+        x1: xcomp-25,
+        x2: xcomp+25,
+        y1: ycomp-25,
+        y2: ycomp+25,
+      };
+      alienArray.push(alien);
+    }
+  }
+}
+
+
