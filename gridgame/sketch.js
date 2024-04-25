9b6075234a0686bbb04758484e57842b33467217
MuhammadAbdullah83u09485107 <161519970+MuhammadAbdullah83u09485107@users.noreply.github.com>
2024-04-24 18:41:13 -0600

    did the html and css for the tic tac toe

---
+++ gridgame/sketch.js
@@ -0,0 +1,57 @@
+//Muhammad Abdullah
+//April 24, 2024
+//Tic Tac Toe Game
+// extra for experts: ???
+
+const cells = document.querySelectorAll(".cell");
+const statusText = document.querySelector("#statusText");
+const restartButton = document.querySelector("#restartButton");
+const winConditions = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+];
+let options = ["", "", "", "", "", "", "", "", "",];
+let currentPlayer = "X";
+let running = false;
+
+function setup() {
+  startGame();
+
+}
+
+
+function draw() {
+
+}
+
+function startGame() {
+  cells.forEach(cell => cell.addEventListener("click", ))
+  restartButton.addEventListener("click", restartGame);
+  statusText.textContent = 
+}
+
+function squareClicked() {
+
+}
+
+function updateSquare(cell, index) {
+
+}
+
+function changePlayer() {
+
+}
+
+function checkWinner() {
+
+}
+
+function restartGame() {
+
+}
